33efe476cc286c70b7a725d8596fc6549e48aa01
Edouard Shaar <edouard.shaar@gsoft.com>
2019-12-05 18:15:20 -0500

    part 2 of 2019/01

--- 2019/01/program.py
+++ 2019/01/program.py
@@ -1,10 +1,19 @@
 import math
 
+def calculateFuel(mass):
+  return math.floor(mass / 3) - 2
+
 fuel = 0
 
 i = open("./input.txt")
 for mass in i:
-  fuel += math.floor(int(mass) / 3) - 2
+  newFuel = calculateFuel(int(mass))
+  fuelForFuel = newFuel
+  while fuelForFuel > 0:
+    fuelForFuel = calculateFuel(fuelForFuel)
+    if fuelForFuel > 0:
+      newFuel += fuelForFuel
+  fuel += newFuel
 i.close()
 
 print(fuel)
