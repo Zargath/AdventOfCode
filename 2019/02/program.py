6b628033c4901fc8f13260afd955ec450c975766
Edouard Shaar <edouard.shaar@gsoft.com>
2019-12-05 20:29:33 -0500

    finished part 2 2019/02

--- 2019/02/program.py
+++ 2019/02/program.py
@@ -1,27 +1,46 @@
-import copy
+from copy import copy
 
-i = open("input.txt")
-program = i.readline().split(',')
-i.close()
+def readProgram():
+  i = open("input.txt")
+  program = i.readline().split(',')
+  i.close()
 
-program = [int(i) for i in program]
-memory = copy.copy(program)
+  return [int(i) for i in program]
 
-address = 0
-while address < len(memory):
-  opcode = memory[address]
-  
-  if opcode == 99:
-    print(memory[0])
-    break
+def run(input):
+  address = 0
+  while address < len(memory):
+    opcode = memory[address]
+    
+    if opcode == 99:
+      return memory[0]
 
-  param1 = memory[memory[address+1]]
-  param2 = memory[memory[address+2]]
+    param1 = memory[memory[address+1]]
+    param2 = memory[memory[address+2]]
 
-  if opcode == 1:
-    memory[memory[address+3]] = param1 + param2
-  
-  if opcode == 2:
-    memory[memory[address+3]] = param1 * param2
-  
-  address += 4
+    if opcode == 1:
+      memory[memory[address+3]] = param1 + param2
+    
+    if opcode == 2:
+      memory[memory[address+3]] = param1 * param2
+    
+    address += 4
+
+program = readProgram()
+memory = copy(program)
+
+print(run(memory))
+
+for param1 in range(0, 100):
+  for param2 in range(0, 100):
+    memory[1] = param1
+    memory[2] = param2
+    try:
+      result = run(memory)
+    except:
+      result = 0
+
+    if result == 19690720:
+      print(f'Found! -- Param1: {param1} Param2: {param2}')
+    else: 
+      memory = copy(program)
